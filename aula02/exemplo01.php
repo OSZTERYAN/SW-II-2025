c5f89289e2a4d9c2957a2b26510310683ea6d69e
OSZTERYAN <161627621+OSZTERYAN@users.noreply.github.com>
2025-02-13 18:26:54 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="pt-br">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi Anderson</p>
+     <?php
+     $nome = "Anderson";
+     echo "<p>Oi, anderson</p>";
+     echo "<p>Oi, $nome </p>";
+     echo "<p>Oi, ".$nome."</p>";
+     ?>
+</body>
+</html>
